1c68dddbc67dd62dabbc07f0b590bd1d6730c050
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-01-31 02:49:50 +0100

    Overdraft Account

---
+++ src/gr/aueb/cf/ch15/accounts/model/OverdraftAcount.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.ch15.accounts.model;
+
+import gr.aueb.cf.ch15.accounts.exceptions.InsufficientBalanceException;
+import gr.aueb.cf.ch15.accounts.exceptions.SsnNotValidException;
+
+public class OverdraftAcount extends  Account{
+
+    public OverdraftAcount() {
+    }
+
+    public OverdraftAcount(String iban, User holder, double balance) {
+        super(iban, holder, balance);
+    }
+
+    @Override
+    public void withdraw(double amount, String ssn) throws SsnNotValidException {
+        try {
+            if (!isSsnValid(ssn)) {
+                throw new SsnNotValidException(ssn);
+            }
+            setBalance(getBalance() - amount);
+
+        } catch (SsnNotValidException e ) {
+            System.err.println("Error " + e.getMessage());
+            throw e;
+        }
+    }
+}
