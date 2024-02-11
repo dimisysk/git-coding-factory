666b204e307ef9182eb68b1d8889ec3021847150
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-02-11 04:27:58 +0100

    IWelcome

---
+++ src/gr/aueb/cf/ch16/functional/IWelcome.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch16.functional;
+
+@FunctionalInterface
+public interface IWelcome { {}
+    void sayHello();
+}
+
+/*
+ * java 7  abstract methods  + static final fields
+ * java 8 > static methods ,  default methods
+ * java 9 > static methods ,  private methods
+ */
