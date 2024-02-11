3cd2d9d9ea8b0e00af7a409372d89d021513b79d
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-02-11 03:32:43 +0100

    Product

---
+++ src/gr/aueb/cf/ch15/products/model/Product.java
@@ -0,0 +1,82 @@
+package gr.aueb.cf.ch15.products.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * JavaBean -> Data Class
+ */
+public class Product implements IProduct {
+    private final static List<IProduct> products = new ArrayList<>();
+
+    private long id;
+    private String productName;
+    private String description;
+    private double price;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(long id, String productName, String description, double price, int quantity) {
+        this.id = id;
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getter & Setters
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public static List<IProduct> getProducts() {
+        return Collections.unmodifiableList(products);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public void insert() {
+        products.add(this);
+    }
+
+}
