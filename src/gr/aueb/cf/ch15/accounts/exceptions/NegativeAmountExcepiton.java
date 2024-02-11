69799330d637d9ae21e15b89cc93ae51f1d8e5ba
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-02-11 03:35:33 +0100

    NegAmountException

--- src/gr/aueb/cf/ch15/accounts/exceptions/NegativeAmountExcepiton.java
+++ src/gr/aueb/cf/ch15/accounts/exceptions/NegativeAmountExcepiton.java
@@ -3,9 +3,9 @@ package gr.aueb.cf.ch15.accounts.exceptions;
 public class NegativeAmountExcepiton extends Exception{
     private static final long serialVersionUID = 3245L;
 
-    public NegativeAmountExcepiton() {
-        super("Amount insufficient");
-    }
+//    public NegativeAmountExcepiton() {
+//        super("Amount insufficient");
+//    }
 
     public NegativeAmountExcepiton(double amount) {
         super("Amount " + amount + "is not sufficient.");
