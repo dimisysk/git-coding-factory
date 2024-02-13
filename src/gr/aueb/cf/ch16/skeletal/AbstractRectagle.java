4d5e2692df944f8ea40c5c400fe6f438c169bdfc
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-02-13 03:12:42 +0100

    chapter 16

---
+++ src/gr/aueb/cf/ch16/skeletal/AbstractRectagle.java
@@ -0,0 +1,48 @@
+package gr.aueb.cf.ch16.skeletal;
+
+public abstract class AbstractRectagle implements IRectangle {
+    private double width;
+    private double height;
+
+    public AbstractRectagle() {
+    }
+
+    public AbstractRectagle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public boolean isSquare() {
+        return width == height;
+    }
+
+   @Override
+    public abstract String toString();
+}
