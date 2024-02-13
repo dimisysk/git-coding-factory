97ff6f6c4845a792ef714bb12f7bf86374951f57
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-02-13 03:26:05 +0100

    chapter 2

---
+++ src/gr/aueb/cf/ch2/ch3/test/MenuApp.java
@@ -0,0 +1,38 @@
+package gr.aueb.cf.ch2.ch3.test;
+
+import java.util.Scanner;
+
+/**
+ *  Δημιουργία ενός προγράμματος
+ *  Μενού επιλογών.
+ */
+public class MenuApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int userIput =  0;
+        do {
+            System.out.println("Παρακαλώ Δώστε Την Επιλογή Που επιθυμείτε.\n");
+            System.out.println("1. Εισαγωγή\n");
+            System.out.println("2. Διαγραφή\n");
+            System.out.println("3. Ενημέρωση\n");
+            System.out.println("4. Αναζήτηση\n");
+            System.out.println("5. Εξοδος\n");
+            userIput = input.nextInt();
+            if (userIput == 1) {
+                System.out.println("Η επιλογή σας είναι Εισαγωγή\n");
+            } else if (userIput == 2) {
+                System.out.println("Η επιλογή σας είναι Διαγραφή\n");
+            } else if (userIput == 3) {
+                System.out.println("Η επιλογή σας είναι Ενημέρωση\n");
+            } else if (userIput == 4) {
+                System.out.println("Η επιλογή σας είναι Αναζήτηση\n");
+            } else if (userIput == 5) {
+                System.out.println("Η επιλογή σας είναι Εξοδος\n");
+            } else if ( userIput >= 5 || userIput <= 0) {
+                System.out.println("Η επιλογή σας είναι μη έγκυρη.");
+            }
+
+        } while ( userIput != 5 );
+    }
+}
+
---
+++ src/gr/aueb/cf/ch2/ch3/test/Test.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.ch2.ch3.test;
+
+public class Test {
+
+
+
+    public static void main(String[] args) {
+
+        boolean isLeap = false;
+        int i = 0;
+
+        while (i <= 2400) {
+            if (((i % 4 == 0) && (i % 100 != 0)) || (i % 400 == 0)) {
+                isLeap = true;
+                System.out.printf("Το έτος που δώσατε ( %d ) είναι δίσεκτο\n", i);
+            } else  {
+                System.out.printf("Το έτος που δώσατε ( %d ) δεν είναι δίσεκτο\n ", i);
+            }
+            i++;
+
+        }
+
+    }
+}
